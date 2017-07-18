045310ae4244a659a46a117a61190b803a0ea823
darinisawesome <github@darinisawesome.com>
2017-07-18 06:59:57 -0700

    Completed first two steps
    Added line of code for changing NAs in "embarked" column to "S"
    
    Added line of code for changing NAs in "age" column to mean of all other ages

--- Refine 2.R
+++ Refine 2.R
@@ -7,3 +7,11 @@ library(tidyr)
 #load titanic working dataset
 titanic <- read_csv("~/Github/Springboard_Foundations_Data-Refine_02/titanic_original.csv")
 
+#1: Port of embarkation
+#Change NA in "embarked" column to "S"
+titanic$embarked[is.na(titanic$embarked)] <- "S"
+
+#2: Age
+#Change NA in "age" column to the mean of all other ages
+titanic$age[is.na(titanic$age)] <- mean(titanic$age, na.rm = TRUE)
+
