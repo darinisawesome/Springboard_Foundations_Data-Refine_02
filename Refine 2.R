d9952089eb62ab8f1f09f393f0f2d4f5dd5bd2fd
darinisawesome <github@darinisawesome.com>
2017-07-18 06:33:18 -0700

    Getting started
    Loaded necessary libraries
    
    Loaded the titanic dataset to work on

---
+++ Refine 2.R
@@ -0,0 +1,9 @@
+#load necessary libraries
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(tidyr)
+
+#load titanic working dataset
+titanic <- read_csv("~/Github/Springboard_Foundations_Data-Refine_02/titanic_original.csv")
+
