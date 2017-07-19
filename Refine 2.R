c5cc64ad17d739b16acfa6844727386792c4c082
darinisawesome <github@darinisawesome.com>
2017-07-19 10:28:58 -0700

    Changed some code segments
    Parts 1, 2, and 4 were changed to dplyr chaining style examples provided by Raghunandan Patthar, my Springboard Foundations of Data Science course mentor.
    
    My previous, mostly base R code has been commented out.

--- Refine 2.R
+++ Refine 2.R
@@ -9,11 +9,20 @@ titanic <- read_csv("~/Github/Springboard_Foundations_Data-Refine_02/titanic_ori
 
 #1: Port of embarkation
 #Change NA in "embarked" column to "S"
-titanic$embarked[is.na(titanic$embarked)] <- "S"
+#titanic$embarked[is.na(titanic$embarked)] <- "S"
+
+# a way to do whis with chaining and dplyr
+titanic <- titanic %>%
+  mutate(embarked = replace(embarked, is.na(embarked), "S")) %>%
+  mutate(embarked = replace(embarked, embarked=="", "S"))
 
 #2: Age
 #Change NA in "age" column to the mean of all other ages
-titanic$age[is.na(titanic$age)] <- mean(titanic$age, na.rm = TRUE)
+#titanic$age[is.na(titanic$age)] <- mean(titanic$age, na.rm = TRUE)
+
+# doing #2 with chaining and dplyr
+titanic <- titanic %>%
+  mutate(age = replace(age, is.na(age), mean(age, na.rm=TRUE)))
 
 #3: Lifeboat
 #Change NA in "boat" column to a string "None" (an "NA" string seemed confusing)
@@ -23,10 +32,16 @@ titanic$boat[is.na(titanic$boat)] <- "None"
 #Create a new column called "has_cabin_number" that is 1 if TRUE and 0 if FALSE
 
 #First, I replaced NA values with "None" strings to make the values more easily workable.
-titanic$cabin[is.na(titanic$cabin)] <- "None"
+#titanic$cabin[is.na(titanic$cabin)] <- "None"
 
 #Then, I added the column with a 1 if the cabin was not "None" and a 0 if it was
-titanic <- titanic %>% mutate(has_cabin_number = as.numeric(titanic$cabin != "None"))
+#titanic <- titanic %>% mutate(has_cabin_number = as.numeric(titanic$cabin != "None"))
+
+# doing #4 with chaining and dplyr
+boat_clean <- age_clean %>%
+  mutate(boat = replace(boat, is.na(boat), "None"))
+cabin_clean <- boat_clean %>%
+  mutate(has_cabin_number=as.integer(!is.na(cabin)))
 
 #Output to a clean file
 write_csv(titanic, "titanic_clean.csv")
